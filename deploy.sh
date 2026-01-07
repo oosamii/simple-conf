16255eed004f042700209bb7371e644cffa9c93d
kiritoaftab <aftabemir123@gmail.com>
2026-01-07 11:22:56 +0530

    deploy.sh init

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+echo "Deploying Frontend..."
+# Add deployment commands here
+cd /home/simple-conf
+git pull origin master
+cd /home/simple-conf/apps/web
+npm install
+npm run build
+systemctl restart simple
+echo "Frontend Deployed Successfully."
+systemctl status simple --no-pager
+
+echo "Deploying Backend..."
+# Add deployment commands here
+cd /home/simple-conf/apps/api
+git pull origin master
+npm install
+npm run dev
+systemctl restart simplebackend
+echo "Backend Deployed Successfully."
+systemctl status simplebackend --no-pager
+
+exit 0
